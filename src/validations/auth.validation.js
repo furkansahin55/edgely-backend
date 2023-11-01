e4022c825a21863aa5aac60b66d2e6b6a8d5d7af
Furkan Sahin <furkansahin55000@gmail.com>
2023-11-02 01:17:27 +0300

    upd

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -10,7 +10,7 @@ const nonce = {
   body: Joi.object().keys({
     message: Joi.object({
       domain: Joi.string().required(),
-      address: Joi.string().hex().length(42).required(),
+      address: Joi.string().length(42).required(),
       statement: Joi.string().required(),
       uri: Joi.string().uri().required(),
       version: Joi.string().required(),
